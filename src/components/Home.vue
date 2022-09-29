e5effe74f00756aacf950c26bdd72b1c29deb523
Andrewh18 <111818262+hall657@users.noreply.github.com>
2022-09-29 18:39:30 -0400

    bubble and Home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,24 @@
 <template>
-    <div>
+    <div id="Home">
+        <bubble :color="lightOrange" :size="small"/>
+        <bubble :color="teal" :size="big"/>
+        <bubble :color="darkBlue" :size="medium"/>
     </div>
 </template>
 
 <script>
+    import bubble from './bubble.vue'
     /* eslint-disable */
     export default {
         name: 'Home',
         data() {
             return {
+                lightOrange: "#fa991c",
+                darkBlue: "#032539",
+                teal: "#1c768f",
+                small: "5em",
+                medium: "10em",
+                big: "20em",
             }
         },
         methods: {
@@ -17,10 +27,18 @@
         },
         computed: {
         },
-        mounted: function () {
+        components: {
+            bubble,
         },
+        mounted: function () {
+        }
     }
 </script>
 
 <style>
+    #Home {
+        height: 100vh;
+        width: 100vw;
+        background: white;
+    }
 </style>
---
+++ src/components/bubble.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="bubble" :style="[getColor(), getSize()]">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'templatePage',
+        data() {
+            return {
+            }
+        },
+        methods: {
+            getColor() {
+                return {
+                    background: this.color
+                }
+            },
+            getSize() {
+                return {
+                    height: this.size,
+                    width: this.size,
+                }
+            }
+        },
+        props: {
+            color: String,
+            size: String,
+        },
+        computed: {
+        },
+        mounted: function () {
+        },
+    }
+</script>
+
+<style>
+    .bubble {
+        border-radius: 50%;
+        opacity: 0.65;
+    }
+</style>
