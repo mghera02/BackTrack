131b7bf749e4a6d24e4029e7d85214aa93a764e6
Andrewh18 <111818262+hall657@users.noreply.github.com>
2022-10-06 20:05:46 -0400

    Added log in button

---
+++ src/components/logInButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="logInButton" :style="[getColor()]">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'logInButton',
+        data() {
+            return {
+            }
+        },
+        methods: {
+            getColor() {
+                return {
+                    background: this.color,
+                }
+            },
+        },
+        props: {
+            color: String,
+        },
+        computed: {
+        },
+        mounted: function () {
+        },
+    }
+</script>
+
+<style>
+    .logInButton {
+        width: 14vw;
+        height: 3vw;
+        position: relative;
+        left: 15vw;
+        top: 19vw;  
+        margin-bottom: 1vw;
+    }
+</style>
