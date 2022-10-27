2eea2ed8a565880a85d891ba6dbb8086da730dd9
Andrewh18 <111818262+hall657@users.noreply.github.com>
2022-10-27 19:50:23 -0400

    worked on contact page

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,8 +2,13 @@
     <div id="Contact">
         <div id="WhiteBox">
             <bubble :color="lightOrange" :size="small" :zLevel='1' :pos="['20vh', '10vw']"/>
-            <bubble :color="teal" :size="big" :zLevel='0' :pos="['25vh', '8vw']"/>
-            <bubble :color="darkBlue" :size="medium" :zLevel='1' :pos="['40vh', '10vw']"/>
+            <bubble :color="teal" :size="big" :zLevel='0' :pos="['25vh', '4vw']"/>
+            <bubble :color="darkBlue" :size="medium" :zLevel='1' :pos="['40vh', '8vw']"/>
+        </div>
+        <div id="Header">
+            Contact us
+        </div>
+        <div id="Gmail">
         </div>
     </div>
 </template>
@@ -20,7 +25,6 @@
                 small: "5vw",
                 medium: "7vw",
                 big: "10vw",
-                halloween: "#EB6123",
             }
         },
         methods: {
@@ -38,12 +42,12 @@
 </script>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
     #Contact {
         position: absolute;
         height: 100vh;
         width: 100vw;
-        background: #1c768f;
-        opacity: 0.65;
+        background: #637b8a;
         display: flex;
         align-items: center;
         justify-content: center;
@@ -56,5 +60,21 @@
         justify-content: center;
         height: 70vh;
         width: 40vw;
+    }
+    #Header {
+        color: black;
+        font-size: 3.15vw;
+        font-family: 'Roboto', sans-serif;
+        position: relative;
+        top: -4vh;
+        left: -24vw;
+    }
+    #Gmail {
+/*        width: 50%;
+        height: 50%;
+        background-image: url('../assets/gmail.png');
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;*/
     }
 </style>
