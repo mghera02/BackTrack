5625017f13e3419180812d6fbb4b249e39aee58c
mghera02 <mghera02@hotmail.com>
2022-11-17 18:32:04 -0500

    start on scan

--- src/components/Scanning/centerRecorder.vue
+++ src/components/Scanning/centerRecorder.vue
@@ -1,6 +1,12 @@
 <template>
     <div id="centerRecorder">
-        <div id="backDrop"></div>
+        <div id="backDrop">
+            <div id="startBtn">
+                <div id="bigText">
+                    Start
+                </div>
+            </div>
+        </div>
         <sideCube :pos='["-30px","-30px"]' :color="color"/>
         <sideCube :pos='["-30px","480px"]' :color="color"/>
         <sideCube :pos='["480px","-30px"]' :color="color"/>
@@ -15,7 +21,7 @@
         name: 'centerRecorder',
         data() {
             return {
-                color: 'white'
+                color: 'red'
             }
         },
         methods: {
@@ -33,6 +39,7 @@
 </script>
 
 <style>
+    @import url('https://fonts.googleapis.com/css2?family=Raleway&display=swap');
     #centerRecorder {
         position:relative;
         background: white;
@@ -50,5 +57,31 @@
         z-index:2;
         border: solid;
         border-color: #999;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    #startBtn{
+        width:300px;
+        height:300px;
+        border-radius: 50%;
+        border: 30px solid;
+        border-color: #00698A;
+        background:white;
+        position:relative;
+        text-align:center;
+        cursor: pointer;
+    }
+    
+    #bigText {
+        transform: scaleX(1.25);
+        font-size: 5em;
+        font-weight: 600;
+        font-family: 'Raleway', sans-serif;
+        text-shadow: 1px 6px 4px #333;
+        color: black;
+        position:relative;
+        top:100px;
     }
 </style>
--- src/components/Scanning/sideCube.vue
+++ src/components/Scanning/sideCube.vue
@@ -78,85 +78,85 @@
     }
     @keyframes greenFadeAnim {
         0%   { box-shadow: 
-            0px 0px 15px 5px #04b233,
-            0px 0px 15px 10px #02a030,
-            0px 0px 15px 20px #029023,
-            0px 0px 15px 25px #008010
+            0px 0px 30px 5px #04b233,
+            0px 0px 30px 10px #02a030,
+            0px 0px 30px 20px #029023,
+            0px 0px 30px 25px #008010
         }
         50%   { box-shadow: 
-            0px 0px 7px 2.5px #04b233,
-            0px 0px 7px 5px #02a030,
-            0px 0px 7px 10px #029023,
-            0px 0px 7px 12.5px #008010
+            0px 0px 10px 2.5px #04b233,
+            0px 0px 10px 5px #02a030,
+            0px 0px 10px 10px #029023,
+            0px 0px 10px 12.5px #008010
         }
         100%   { box-shadow: 
-            0px 0px 15px 5px #04b233,
-            0px 0px 15px 10px #02a030,
-            0px 0px 15px 20px #029023,
-            0px 0px 15px 25px #008010
+            0px 0px 30px 5px #04b233,
+            0px 0px 30px 10px #02a030,
+            0px 0px 30px 20px #029023,
+            0px 0px 30px 25px #008010
         }
     }
 
     @keyframes redFadeAnim {
         0%   { box-shadow: 
-            0px 0px 15px 5px #b23030,
-            0px 0px 15px 10px #a02020,
-            0px 0px 15px 20px #901010,
-            0px 0px 15px 25px #800000
+            0px 0px 30px 5px #b23030,
+            0px 0px 30px 10px #a02020,
+            0px 0px 30px 20px #901010,
+            0px 0px 30px 25px #800000
         }
         50%   { box-shadow: 
-            0px 0px 7px 2.5px #b23030,
-            0px 0px 7px 5px #a02020,
-            0px 0px 7px 10px #901010,
-            0px 0px 7px 12.5px #800000
+            0px 0px 10px 2.5px #b23030,
+            0px 0px 10px 5px #a02020,
+            0px 0px 10px 10px #901010,
+            0px 0px 10px 12.5px #800000
         }
         100%   { box-shadow: 
-            0px 0px 15px 5px #b23030,
-            0px 0px 15px 10px #a02020,
-            0px 0px 15px 20px #901010,
-            0px 0px 15px 25px #800000
+            0px 0px 30px 5px #b23030,
+            0px 0px 30px 10px #a02020,
+            0px 0px 30px 20px #901010,
+            0px 0px 30px 25px #800000
         }
     }
 
     @keyframes yellowFadeAnim {
         0%   { box-shadow: 
-            0px 0px 15px 5px #fafa63,
-            0px 0px 15px 10px #f0f053,
-            0px 0px 15px 20px #eaea43,
-            0px 0px 15px 25px #e0e043
+            0px 0px 30px 5px #fafa63,
+            0px 0px 30px 10px #f0f053,
+            0px 0px 30px 20px #eaea43,
+            0px 0px 30px 25px #e0e043
         }
         50%   { box-shadow: 
-            0px 0px 7px 2.5px #cccc10,
-            0px 0px 7px 5px #bcbc00,
-            0px 0px 7px 10px #acac00,
-            0px 0px 7px 12.5px #9c9c00
+            0px 0px 10px 2.5px #cccc10,
+            0px 0px 10px 5px #bcbc00,
+            0px 0px 10px 10px #acac00,
+            0px 0px 10px 12.5px #9c9c00
         }
         100%   { box-shadow: 
-            0px 0px 15px 5px #fafa63,
-            0px 0px 15px 10px #f0f053,
-            0px 0px 15px 20px #eaea43,
-            0px 0px 15px 25px #e0e043
+            0px 0px 30px 5px #fafa63,
+            0px 0px 30px 10px #f0f053,
+            0px 0px 30px 20px #eaea43,
+            0px 0px 30px 25px #e0e043
         }
     }
 
     @keyframes whiteFadeAnim {
         0%   { box-shadow: 
-            0px 0px 15px 6px #ccc,
-            0px 0px 15px 8px #eee,
-            0px 0px 15px 10px #fff,
-            0px 0px 15px 12px #fff
+            0px 0px 20px 6px #ccc,
+            0px 0px 20px 8px #eee,
+            0px 0px 20px 10px #fff,
+            0px 0px 20px 12px #fff
         }
         50%   { box-shadow: 
-            0px 0px 5px 1px #999,
-            0px 0px 5px 2px #aaa,
-            0px 0px 5px 3px #bbb,
-            0px 0px 5px 4px #ccc
+            0px 0px 2px 0.25px #999,
+            0px 0px 2px 0.5px #aaa,
+            0px 0px 2px 0.75px #bbb,
+            0px 0px 2px 1px #ccc
         }
         100%   { box-shadow: 
-            0px 0px 15px 6px #ccc,
-            0px 0px 15px 8px #eee,
-            0px 0px 15px 10px #fff,
-            0px 0px 15px 12px #fff
+            0px 0px 20px 6px #ccc,
+            0px 0px 20px 8px #eee,
+            0px 0px 20px 10px #fff,
+            0px 0px 20px 12px #fff
         }
     }
 </style>
