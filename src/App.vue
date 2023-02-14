0cc53ca4b3d0cfd1ac5435e79f5b9c0b28a0fea2
juli3k <gkim6735@gmail.com>
2023-02-13 19:17:09 -0500

    added mission statement page

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
   import workOutPage from './components/WorkOut/WorkOutPage.vue'
   import contact from './components/Contact/Contact.vue'
   import scanning from './components/Scanning/Scanning.vue'
+  import MissionStatement from './components/MissionStatement/MissionStatement.vue'
   
   const routes = {
     '': Home,
@@ -14,6 +15,7 @@
     '/workOutPage': workOutPage,
     '/Contact': contact,
     '/Scanning': scanning,
+    '/MissionStatement': MissionStatement,
   }
   
   export default {
@@ -33,48 +35,25 @@
       })
     }
   }
-</script>
-
-<script setup>
-  /* eslint-disable */
-  import { ref, watchEffect } from 'vue' // used for conditional rendering
-  import firebase from 'firebase/compat/app';
-  import 'firebase/compat/auth';
-  import 'firebase/compat/firestore';
-  import { useRouter } from 'vue-router'
-  const router = useRouter()
-  const isLoggedIn = ref(true)
-  // runs after firebase is initialized
-  firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        isLoggedIn.value = true // if we have a user
-      } else {
-        isLoggedIn.value = false // if we do not
-      }
-  })
-  const signOut = () => {
-    firebase.auth().signOut()
-    router.push('/')
-  }
-</script>
-
-<template>
-  <component :is="currentView" />
-</template>
-
-<style>
-  html, body {
-    height: 100vh;
-    width: 100vw;
-    border: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background: black;
-    color: white;
-  }
-  #app {
-    width:100%;
-    height:100%;
-  }
-</style>
+  </script>
+  
+  <template> 
+    <component :is="currentView" />
+  </template>
+  
+  <style>
+    html, body {
+      height: 100vh;
+      width: 100vw;
+      border: none;
+      margin: 0;
+      padding: 0;
+      overflow: hidden;
+      background: black;
+      color: white;
+    }
+    #app {
+      width:100%;
+      height:100%;
+    }
+  </style>
